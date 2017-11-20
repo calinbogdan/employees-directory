caed75de22a32a8032353efa2b46827bdca58c22
Bogdan Călin <calin.bogdan15@gmail.com>
2017-11-20 21:43:35 +0200

    Created Employee class

--- EmployeesAPI/Data/Context.cs
+++ EmployeesAPI/Data/Context.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace EmployeesAPI.Data
 {
-    public class Context
+    public class Context : DbContext
     {
-        
+        public Context(DbContextOptions<Context> options) : base(options) {}
+
+        public DbSet<Employee> Employees { get; set; }
     }
 }
