8796c466ec1a151a7e18e815386a1fa85b487595
Bogdan Călin <calin.bogdan15@gmail.com>
2017-11-21 07:26:40 +0200

    Created Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,85 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router';
+
+import Home from './components/pages/Home';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      employees: [],
-      employee: {
-        name: null,
-        email: null
-      }
-    }
-  }
-  componentWillMount() {
-    this.getEmployeesAsync();
-  }
-
-  async getEmployeesAsync() {
-    fetch('http://localhost:5000/employees/')
-    .then(response => response.json())
-    .then(responseJson => {
-      this.setState({
-        employees: responseJson
-      });
-    });
-  }
-
-  handleNameChange(e) {
-    this.setState({
-      employee: {
-        name: e.target.value,
-        email: this.state.employee.email
-      }
-    });
-  }
-
-  handleEmailChange(e) {
-    this.setState({
-      employee: {
-        name: this.state.employee.name,
-        email: e.target.value
-      }      
-    })
-  }
-
-  handleFormSubmit(e) {
-    e.preventDefault();
-    fetch('http://localhost:5000/employees/create', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name: this.state.employee.name,
-        email: this.state.employee.email
-      })
-    })
-    .then(response => response.json())
-    .then(responseJson => {
-      console.log(responseJson);
-    })
-
-    this.getEmployeesAsync();
-  }
 
   render() {
     return (
       <div>
-        <ul>
-          {this.state.employees.map(employee => {
-            return <li>{employee.name} - {employee.email}</li>
-          })}
-        </ul>
-        <form onSubmit={e => this.handleFormSubmit(e)}>
-          <input type="text" onChange={e => this.handleNameChange(e)}/>
-          <input type="text" onChange={e => this.handleEmailChange(e)}/>
-          
-          <input type="submit" value="Submit"/>
-        </form>
+        <BrowserRouter>
+          <div>
+            {/* <Navigation/> */}
+            <Switch>
+              <Route exact path='/' component={Home} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            employees: []
+        }
+    }
+
+    componentWillMount() {
+        this.getEmployeesAsync();    
+    }
+
+    async getEmployeesAsync() {
+        fetch('http://localhost:5000/employees/get')
+        .then(response => response.json())
+        .then(employees => {
+            this.setState({
+                employees: employees
+            });
+        })
+        .catch(error => console.log(error));
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.state.employees.map((index, employee) => {
+                        return (
+                        <li key={index}>
+                            <h1>Name: {employee.name}</h1>
+                            <h4>Email: {employee.email}</h4>
+                        </li>);
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
